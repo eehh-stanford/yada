7448b1ff479429a8b6c838f549e295f258d83dec
Michael Holton Price <mpatmudd@yahoo.com>
2018-05-21 11:47:29 -0600

    Created functions to calculate the standard things for a distribution (d,p,q,r) as well as the hazard and cumulative hazard for the Siler hazard model

---
+++ R/siler.R
@@ -0,0 +1,95 @@
+#' @title Siler hazard model
+#'  
+#' @description \code{dsiler} gives the probability density function, \code{psiler} the cumulative density function, \code{qsiler} the quantile function, \code{rsiler} random draws, \code{hsiler} the hazard, \code{chsiler} the cumulative hazard, and \code{ssiler} the survival.
+#' 
+#' @details The Siler hazard is
+#' 
+#' \deqn{\lambda(x) = a_1 e^{-a_2 x} + a_3 + a_4 e^{a_5 x}}
+#' 
+#' where \eqn{a_i} (index notation) is the parameter vector. The cumulative hazard is found by integrating the hazard from some initial point \eqn{x_0} to \eqn{x},
+#' 
+#' \deqn{\Lambda(x_0,x) = -\frac{a_1}{a_2} (e^{-a_2 x} - e^{-a_2 x_0}) + a_3 (x-x_0) + \frac{a_4}{a_5} (e^{a_5 x} - e^{a_5 x_0})}.
+#' 
+#' If \eqn{x_0} (optional) is not input it is set equal to 0. The survival and cumulative density function are, respectively,
+#'
+#' \deqn{S(x_0,x) = e^{-\Lambda(x_0,x)}}
+#'
+#' and
+#'
+#' \deqn{F(x_0,x) = 1 - S(x_0,x)}.
+#'
+#' The probability density function is
+#'
+#' \deqn{f(x_0,x) = \frac{d}{dx} F(x_0,x) = \lambda(x) S(x_0,x)}.
+#'
+#' @param x,q vector of quantiles
+#' @param p vector of probabilities
+#' @param n number of observations
+#' @param a parameterization vector
+#' 
+#' @keywords Siler, hazard, survival, demography
+#' @export
+#' 
+# @examples
+# 
+#' @return A numeric vector of length equal to the input
+#
+#' @author Michael Holton Price <MichaelHoltonPrice@gmail.com>
+# @seealso 
+# @references 
+#
+#' @rdname Siler
+#'
+
+hsiler <- function(x,a) {
+    return(a[1]*exp(-a[2]*x) + a[3] + a[4]*exp(a[5]*x))
+}
+
+
+#' @export
+#' @rdname Siler
+chsiler <- function(x,a,x0=0) {
+    if(x0==0) {
+        return(-a[1]/a[2]*(exp(-a[2]*x)-1) + a[3]*x + a[4]/a[5]*(exp(a[5]*x)-1))
+    } else {
+        return(chsiler(x,a) - chsiler(x0,a))
+    }
+}
+
+#' @export
+#' @rdname Siler
+ssiler <- function(x,a,x0=0) {
+    return(exp(-chsiler(x,a,x0)))
+}
+
+#' @export
+#' @rdname Siler
+dsiler <- function(x,a,x0=0) {
+    return(hsiler(x,a) * ssiler(x,a,x0))
+}
+
+#' @export
+#' @rdname Siler
+psiler <- function(x,a,x0=0) {
+    return(1-ssiler(x,a,x0))
+}
+
+#' @export
+#' @rdname Siler
+qsiler <- function(qvect,a,x0=0,maxVal=1000) {
+    N <- length(qvect)
+    xvect <- rep(NA,N)
+    for(n in 1:N) {
+      Sn <- 1 - qvect[n]
+      froot <- function(x) {log(Sn)+chsiler(x,a,x0)}
+      xvect[n] <- uniroot(froot,c(0,maxVal))$root
+    }
+    return(xvect)
+}
+
+#' @export
+#' @rdname Siler
+rsiler <- function(N,a,x0=0,maxVal=1000) {
+    cdf <- runif(N)
+    return(qsiler(cdf,a,x0,maxVal))
+}

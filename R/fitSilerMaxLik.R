f66cc177299f2e527fd0ec8ecdaf5e40f6432fd7
Michael Holton Price <mpatmudd@yahoo.com>
2018-05-22 18:22:06 -0600

    Added Siler vignette

---
+++ R/fitSilerMaxLik.R
@@ -0,0 +1,40 @@
+#' @title Maximum likelihood fit for Siler distribution
+#'  
+#' @description Do a maximum likelihood Siler fit for the input data vector x. To allow unconstrained optimization to be used, optimization is done on the transformed variable abar = log(a/a0).
+#' 
+# @details
+#'
+#' @param x Locations at which to evaluate probability density function
+#' @param a0 Initial value for optimization. Default from Gage and Dyke 1986, Level 15, Table 2
+#' @param x0 x-offset. The default is 0 (no offset).
+#' 
+# @keywords
+#' @export
+#' 
+# @examples
+#' 
+#' @return A list consisting of the fit (on the transformed variable abar) and maximum likelihood estimate of a. Optionally, the Hessian of the log-likelihood is returned, which allows estimation of the standard errors of the maximum likelihood estimate via the observed Fisher information matrix.
+#'
+#' @author Michael Holton Price <MichaelHoltonPrice@gmail.com>
+#' @seealso \code{dsiler}
+# @references 
+
+fitSilerMaxLik <- function(x,a0=c(.175,1.40,.368*.01,.075*.001,.917*.1),x0=0,calcHessian=FALSE) {
+    nllbar <- function(abar,x,a0) {
+        a <- a0 * exp(abar)
+        -sum(log(dsiler(x,a,x0)))
+    }
+
+    fit <- optim(rep(0,5),nllbar,x=x,a0=a0)
+    
+    afit <- a0*exp(fit$par)
+    if(calcHessian) {
+        ll <- function(a) { # log likelihood as a function of a
+            sum(log(dsiler(x,a,x0)))
+        }
+        H <- numDeriv::hessian(ll,afit)
+        return(list(fit=fit,a=afit,hessian=H))
+    } else {
+        return(list(fit=fit,a=fit))
+    }
+}

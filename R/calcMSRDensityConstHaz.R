5b1226ab87c7a995b00ca754c2d55d39828f9bfb
Michael Holton Price <mpatmudd@yahoo.com>
2018-06-05 14:49:31 -0600

    Added vignette that generates figures in survival goodness-of-fit article (PDF plots only for now). Updated corresonding code.

--- R/calcMSRDensityConstHaz.R
+++ R/calcMSRDensityConstHaz.R
@@ -1,10 +1,10 @@
 #' @title Calculate the Martingale Survival Residual probability density
 #'  
-#' @description Calculate the expected probability density function for the Martingale Survival Residual (MSR). This assumes constant transition intensities for both event occurrence and censoring.
+#' @description Calculate the expected probability density function for the Martingale Survival Residual (MSR). This assumes constant transition intensities (haard) for both event occurrence and censoring.
 #' 
 # @details
 #'
-#' @param y A vector of locations at which to calculate the probability density
+#' @param D A vector of locations at which to calculate the probability density
 #' @param rho The censoring ratio
 #' @param component The component of the probability density to return. "both" yields the full density. "event" yields the component due to event occurrence. "censored" yields the component due to censoring. The default is "both".
 #' 
@@ -13,7 +13,7 @@
 #' 
 # @examples
 #' 
-#' @return A vector of probability densities that is the same length as y
+#' @return A vector of probability densities that is the same length as D
 #'
 #' @author Michael Holton Price <MichaelHoltonPrice@gmail.com>
 #' @seealso calcMSR
@@ -21,11 +21,11 @@
 #' @references 
 #' \url{https://www.biorxiv.org/content/early/2017/01/31/104406}
 
-calcMSRDensity <- function(y,rho,component="both") {
-    f.e <- (.5-y)^(rho/(1-rho)) # event component
-    f.c <- rep(0,length(y)) # censoring component
-    ind <- y >= 0 # Indices of censored component
-    f.c[ind] <-  f.c[ind] + (1-2*y[ind])^(rho/(1-rho))*2*rho/(1-rho)
+calcMSRDensityConstHaz <- function(D,rho,component="both") {
+    f.e <- (.5-D)^(rho/(1-rho)) # event component
+    f.c <- rep(0,length(D)) # censoring component
+    ind <- D >= 0 # Indices of censored component
+    f.c[ind] <-  f.c[ind] + (1-2*D[ind])^(rho/(1-rho))*2*rho/(1-rho)
     f <- f.e + f.c
     if(component=="event") {
         return(f.e)
--- R/calcNMRDensityConstHaz.R
+++ R/calcNMRDensityConstHaz.R
@@ -4,7 +4,7 @@
 #' 
 # @details
 #'
-#' @param y A vector of locations at which to calculate the probability density
+#' @param D A vector of locations at which to calculate the probability density
 #' @param rho The censoring ratio
 #' @param component The component of the probability density to return. "both" yields the full density. "event" yields the component due to event occurrence. "censored" yields the component due to censoring. The default is "both".
 #' 
@@ -13,7 +13,7 @@
 #' 
 # @examples
 #' 
-#' @return A vector of probability densities that is the same length as y
+#' @return A vector of probability densities that is the same length as D
 #'
 #' @author Michael Holton Price <MichaelHoltonPrice@gmail.com>
 #' @seealso calcMartResid
@@ -21,11 +21,11 @@
 #' @references 
 #' \url{https://www.biorxiv.org/content/early/2017/01/31/104406}
 
-calcNMRDensity <- function(y,rho,component="both") {
-    f.e <- exp(-(y+1)/(1-rho)) # event component
-    f.c <- rep(0,length(y))    # censoring component
-    ind <- y >= 0 # Indices of censored component
-    f.c[ind] <- f.c[ind] + exp(-y[ind]/(1-rho)) * rho/(1-rho)
+calcNMRDensityConstHaz <- function(D,rho,component="both") {
+    f.e <- exp(-(D+1)/(1-rho)) # event component
+    f.c <- rep(0,length(D))    # censoring component
+    ind <- D >= 0 # Indices of censored component
+    f.c[ind] <- f.c[ind] + exp(-D[ind]/(1-rho)) * rho/(1-rho)
     f <- f.e + f.c	
     if(component=="event") {
         return(f.e)

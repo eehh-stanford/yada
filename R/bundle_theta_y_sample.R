41d3edc26c54a13e11fb63e5222059246d102fff
MichaelHoltonPrice <MichaelHoltonPrice@gmail.com>
2019-01-25 16:45:04 -0700

    Added ability to bundle and save theta_y sample result

---
+++ R/bundle_theta_y_sample.R
@@ -0,0 +1,41 @@
+#' @title Bundle result of Metropolis sampling into a single convenient list
+#'
+#' @description \code{bundles_theta_y_sample} bundles theta_yList, x, Y, hp, varNames, and known into a single convenient list. This bundled result is used for displaying and saving results.
+#'
+# @details
+#'
+#' @param theta_yList A list of samples of theta_y (samples are a list, not a vector)
+#' @param x Vector of indepenent variable observations
+#' @param Y Matrix of dependent variable observations
+#' @param hp Hyperparameters
+#' @param varNames Vector of variable names [optional]
+#' @param known List object of known simulation values [optional]
+#'
+# @keywords
+#' @export
+#'
+#' @return The bundled list
+#'
+#' @author Michael Holton Price <MichaelHoltonPrice@gmail.com>
+#' @seealso simConstHaz, fitConstHaz, fitLinHaz
+# @references
+
+#' @export
+bundle_theta_y_sample <- function(thetaList,x,Y,hp,varNames=NA,known=NA) {
+  bundle <- list(thetaList=thetaList,Y=Y,x=x,hp=hp)
+  if(!all(is.na(known))) {
+    bundle$known <- known
+    bundle$haveKnown <- T
+  } else {
+    bundle$haveKnown <- F
+  }
+
+  if(!all(is.na(varNames))) {
+    bundle$varNames <- varNames
+    bundle$haveNames <- T
+  } else {
+    bundle$haveNames <- F
+  }
+
+  return(bundle)
+}
---
+++ R/save_theta_y_sample.R
@@ -0,0 +1,28 @@
+#' @title Save a sample of theta_y from the adaptive Metropolis algorithm
+#'
+#' @description \code{save_theta_y_sample} saves to file the result of sampling theta_y using the Metropolis algorithm
+#'
+# @details
+#'
+#' @param fileName The save file
+#' @param thetaList A list of samples of theta_y (samples are a list, not a vector)
+#' @param x Vector of indepenent variable observations
+#' @param Y Matrix of dependent variable observations
+#' @param hp Hyperparameters
+#' @param varNames Vector of variable names [optional]
+#' @param known List object of known simulation values [optional]
+#'
+# @keywords
+#' @export
+#'
+# @examples
+#'
+#' @author Michael Holton Price <MichaelHoltonPrice@gmail.com>
+#' @seealso simConstHaz, fitConstHaz, fitLinHaz
+# @references
+
+#' @export
+save_theta_y_sample <- function(fileName,thetaList,x,Y,hp,varNames=NA,known=NA) {
+  bundle <- bundle_theta_y_sample(thetaList,x,Y,hp,varNames,known)
+  saveRDS(bundle,fileName)
+}

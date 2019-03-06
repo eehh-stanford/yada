d48b2f90d4ab18edf4544f0321bd70301ba247b2
Michael Holton Price <MichaelHoltonPrice@gmail.com>
2019-03-05 23:20:08 -0700

    Prototyped simulated annealing for finding maximum likelihood of mixed probit model

---
+++ R/adaptiveSA.R
@@ -0,0 +1,52 @@
+#' @title Adaptive simulated annealing optimization
+#'
+#' @description This function implements adaptive simulated annealing, in which the adaptation is to modify the proposal distribution using the approach in Harrio, Saksman, and Tamminen (2001), An adaptive Metropolis algorithm, in Bernoulli, Volume 7, Number 2, pages 223 through 242. 
+#'
+#' @param costFunc The cost function to be minimized
+#' @param X_0 The starting point for sampling
+#' @param ... Further arguments to be passed to costFunc
+#' @param control A list of control parameters. See Details.
+#'
+#' @return A list containing the samples along with summary information
+#'
+#' @author Michael Holton Price <MichaelHoltonPrice@gmail.com>
+
+#' @export
+adaptiveSA <- function(costFunc,X_0,...,control=list()) {
+
+  # If necessary, set control defaults
+  if(!('sampControls' %in% names(control))) {
+    sampControls <- list()
+  } else {
+    sampControls <- control$sampControls
+  }
+
+  if(!('tempVect' %in% names(control))) {
+    control$tempVect <- 4*(3.5/4)^(0:12)
+    #control$tempVect <- c(2,1.75,1.5,1.25,1,.75,.25)
+    #control$tempVect <- c(4,2,1)
+  }
+
+  sampList <- list()
+  print('xxxx')
+  print(1)
+  targetDist <- function(X) {exp(-costFunc(X,...)/control$tempVect[1])}
+  sampControls$temp <- control$tempVect[1]
+  sampList[[1]] <- adaptiveMetrop(targetDist,X_0,control=sampControls)
+  saveRDS(sampList,'sampList.rds')
+  for(n in 2:length(control$tempVect)) {
+    print('xxxx')
+    print(n)
+    targetDist <- function(X) {exp(-costFunc(X,...)/control$tempVect[n])}
+    X_0 <- sampList[[n-1]]$X_list
+    X_0 <- X_0[[length(X_0)]]
+    C_0 <- sampList[[n-1]]$C_t * control$tempVect[n] / control$tempVect[n-1]
+    #C_0 <- sampList[[n-1]]$covX
+    #print(C_0)
+    sampControls$C_0 <- C_0
+    sampControls$temp <- control$tempVect[n]
+    sampList[[n]] <- adaptiveMetrop(targetDist,X_0,control=sampControls)
+    saveRDS(sampList,'sampList.rds')
+  }
+  return(list(sampList=sampList,control=control))
+}

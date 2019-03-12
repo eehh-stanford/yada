dbd2aec6df73a734be1b09a382afc46ca09e3baa
MichaelHoltonPrice <MichaelHoltonPrice@gmail.com>
2019-03-12 14:22:55 -0600

    Added code to calculate and plot confidence intervals. Removed export from (untracked) file /R/saTemperInit.R

---
+++ R/analyze_x_posterior.R
@@ -0,0 +1,67 @@
+#' @title Analyze an input density by calculating the mean and confidence intervals
+#'
+#' @description \code{analyze_x_posterior} analyzes an input density given by the input pair of vectors xv and fv by calculating the mean and confidence intervals for a handful of quantiles (0.001, 0.025, 0.5, 0.975, and 0.999). xv is assumed to be evenly spaced. Optionally, a known age (xknown) can be input, for which the density is estimated.
+#'
+# @details
+#'
+#' @param xv A vector of evenly spaced ages at the density, fv, is calculated
+#' @param fv A vector of densities
+#' @param xknown Known age [optional]
+#'
+#'
+#' @return A list containing the results of the analysis
+#'
+#' @author Michael Holton Price <MichaelHoltonPrice@gmail.com>
+
+#' @export
+analyze_x_posterior <- function(xv,fv,xknown=NA) {
+  if(xv[1] != 0) {
+    stop('First element of xv should be zero')
+  }
+  dx <- xv[2] - xv[1]
+  # fv is the same as p_x
+  fv <- fv / sum(fv) / dx
+  Fv <- c(0,dx*cumsum(fv)[1:(length(xv)-1)])
+  
+  q <- 0.001
+  n <- max(which(Fv <= q))
+  xlolo <- xv[n] + dx*(q-Fv[n])/(Fv[n+1]-Fv[n])
+  flolo <- fv[n] + (xlolo-xv[n])*(fv[n+1]-fv[n])/dx
+
+  q <- 0.025
+  n <- max(which(Fv <= q))
+  xlo <- xv[n] + dx*(q-Fv[n])/(Fv[n+1]-Fv[n])
+  flo <- fv[n] + (xlo-xv[n])*(fv[n+1]-fv[n])/dx
+
+  q <- 0.5
+  n <- max(which(Fv <= q))
+  xmed <- xv[n] + dx*(q-Fv[n])/(Fv[n+1]-Fv[n])
+  fmed <- fv[n] + (xmed-xv[n])*(fv[n+1]-fv[n])/dx
+
+  q <- 0.975
+  n <- max(which(Fv <= q))
+  xhi <- xv[n] + dx*(q-Fv[n])/(Fv[n+1]-Fv[n])
+  fhi <- fv[n] + (xhi-xv[n])*(fv[n+1]-fv[n])/dx
+
+  q <- 0.999
+  n <- max(which(Fv <= q))
+  xhihi <- xv[n] + dx*(q-Fv[n])/(Fv[n+1]-Fv[n])
+  fhihi <- fv[n] + (xhihi-xv[n])*(fv[n+1]-fv[n])/dx
+
+  xmean <- sum(fv*xv)*dx
+  n <- max(which(xv < xmean))
+  fmean <- fv[n] + (xmean-xv[n])*(fv[n+1]-fv[n])/dx
+
+  returnList <- list(x=xv,density=fv,dx=dx,xlolo=xlolo,xlo=xlo,xmed=xmed,xhi=xhi,xhihi=xhihi,xmean=xmean,flo=flo,fmed=fmed,fhi=fhi,fmean=fmean)
+  if(!is.na(xknown)) {
+    if(xknown == 0) {
+      fknown <- fv[1]
+    } else {
+      n <- max(which(xv < xknown))
+      fknown <- fv[n] + (fv[n+1] - fv[n]) * (xknown-xv[n]) / dx
+    }
+    returnList$xknown <- xknown
+    returnList$fknown <- fknown
+  }
+  return(returnList)
+}
---
+++ R/calc_joint.R
@@ -0,0 +1,25 @@
+#' @title Calculate the joint probability for the mixed cumulative probit model
+#'
+#' @description \code{calc_joint} calculates the joint density probability p(x,y|theta_x,theta_y) for the mixed cumulative probit model at the points in xcalc. xcalc is assumed to be evenly spaced.
+#'
+# @details
+#'
+#' @param xcalc A vector of ages at which to calculate the posterior probability
+#' @param y An observation of traits used for the posterior calculation
+#' @param theta_x Parameterization for prior on x
+#' @param theta_y Parameterization for likelihood
+#' @param hp Hyperparameters
+#'
+#'
+#' @return A vector of joint probabilities
+#'
+#' @author Michael Holton Price <MichaelHoltonPrice@gmail.com>
+
+#' @export
+calc_joint <- function(xcalc,y,theta_x,theta_y,hp) {
+  logJoint <- log(calc_x_density(xcalc,theta_x))
+  for(n in 1:length(xcalc)) {
+    logJoint[n] <- logJoint[n] + calcLogLik_theta_y(theta_y,xcalc[n],y,hp)
+  }
+  return(exp(logJoint))
+}

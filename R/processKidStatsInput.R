29f2a7462cca95f2e3681ad1956ff843ec7b1206
MichaelHoltonPrice <MichaelHoltonPrice@gmail.com>
2019-03-24 21:09:40 -0600

    Added option to print information about variable mapping in processKidStatsInput

--- R/processKidStatsInput.R
+++ R/processKidStatsInput.R
@@ -3,9 +3,14 @@
 #' @author Michael Holton Price <MichaelHoltonPrice@gmail.com>
 
 #' @export
-processKidStatsInput <- function(kidStatsInput,cumProbitModel,xknown=NA) {
+processKidStatsInput <- function(kidStatsInput,cumProbitModel,xknown=NA,verbose=F) {
   # Extract the input data
   y <- rep(NA,length(cumProbitModel$varNames))
+  if(verbose) {
+    print('--')
+    print('Input variable value list, kidStatsInput$varVal')
+    print(kidStatsInput$varVal)
+  }
   for(v in 1:length(cumProbitModel$varNames)) {
     if(cumProbitModel$varNames[v] %in% names(kidStatsInput$varVal)) {
       if(v <= cumProbitModel$hp$J) {
@@ -13,8 +18,17 @@ processKidStatsInput <- function(kidStatsInput,cumProbitModel,xknown=NA) {
         originalCat = as.numeric(kidStatsInput$varVal[cumProbitModel$varNames[v]])
         yadaCat <- which(cumProbitModel$hp$originalCat[[v]] == originalCat) - 1
         y[v] <- yadaCat
+        if(verbose) {
+          print('-- ordinal --')
+          print(cumProbitModel$varNames[v])
+          print(paste('Input value:',as.character(originalCat)))
+          print(paste('Remapped value for yada:',as.character(yadaCat)))
+	}
       } else {
         # Continuous
+          print('-- continuous --')
+          print(cumProbitModel$varNames[v])
+          print(as.numeric(kidStatsInput$varVal[cumProbitModel$varNames[v]]))
         y[v] <- as.numeric(kidStatsInput$varVal[cumProbitModel$varNames[v]])
       }
     }
